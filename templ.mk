02f1ce02dfa4237f81147d06c2f888bc1dd4c280
Lucas Tan <tbhtan3@gmail.com>
2012-12-29 23:18:03 +0800

    updated templ.mk, added docs rules, updated comments

--- templ.mk
+++ templ.mk
@@ -1,31 +1,61 @@
-# template make file for java
-# The following vars must be predefined:
+# Makefile template for Java.
+# The following vars are available for customizing the build process:
 #
-# PKGS = list of packages. for e.g., java/util  java/net
-#        The packages are assumed to be in the src dir
+# PKGS [Required] :
+#   Space-separated names of packages to compile. 
+#   For e.g., PKGS = com/pkg/util com/pkg/io
+#   The packages are assumed to be in the src dir.
 #
-# LIBS = relative paths to external jar files
+# JAR_FILE_NAME [Required] :
+#   Name of the final jar file.
+#   For e.g., JAR_FILE_NAME = mylib.jar
 #
-# JAR_FILE_NAME = name of final jar file
+# LIBS [Default=none] :
+#   Relative paths (from Makefile) to external jar files.
+#   For e.g., LIBS = logback-1.0/logback.jar libxml/libxml.jar
 #
-# MAIN_CLASS = name of main class along with full package 
-#              path. For e.g., my.pkg.App
-#              Leave blank if don't have.
+# MAIN_CLASS [Default=none] :
+#   Name of main class along with full package path.
+#   For e.g., MAIN_CLASS = com.pkg.App
+#
+# TARGET_VERSION [Default=1.5] :
+#   Target JVM version to compile for. 
+#   For e.g., TARGET_VERSION = 1.5
+#
+# Output vars (created by this template):
+# SRCS :
+#   Space-separated paths to each individual .java file 
+#   For e.g., SRCS = src/com/pkg/Util.java src/com/pkg/InputStream.java
+#
+# CLASSES :
+#   Space-separated paths to each individual compiled .class file 
+#   For e.g., CLASSES = bin/com/pkg/Util.class bin/com/pkg/InputStream.class
+#
+# JAVAC_OPTS : all command-line options to javac
+# JAVADOC_OPTS : all command-line options to javadoc
+#
 
+ifeq ($(strip $(TARGET_VERSION)),)
+    TARGET_VERSION = 1.5
+endif
 
+SRC_DIR = src
 OUT_DIR = bin
+DOCS_DIR = docs
+PATH_SEP = :
+DIR_SEP = / # unused for now since Windows can handle / ?
 
 # TMP1_ = src/j/io/*.java etc....
-TMP1_ = $(addsuffix /*.java,$(addprefix src/,$(PKGS)))
+TMP1_ = $(addsuffix /*.java,$(addprefix $(SRC_DIR)/,$(PKGS)))
 
 # Expand the wildcards to get the actual file names
 SRCS = $(foreach dir,$(TMP1_),$(wildcard $(dir)))
 
 TMP2_ = $(SRCS:.java=.class)
-CLASSES = $(TMP2_:src/%=$(OUT_DIR)/%)
+CLASSES = $(TMP2_:$(SRC_DIR)/%=$(OUT_DIR)/%)
 
-TMP3_ = $(addsuffix :,$(LIBS) src/ bin/)
-LIBS_OPTS = -cp "$(subst : ,:,$(TMP3_))"
+TMP3_ = $(addsuffix $(PATH_SEP),$(LIBS) $(SRC_DIR)/ $(OUT_DIR)/)
+LIBS_OPTS = -cp "$(subst $(PATH_SEP) ,$(PATH_SEP),$(TMP3_))"
 
 LIBS_TARGETS = $(addprefix $(OUT_DIR)/,$(LIBS:.jar=.xxx))
 
@@ -35,32 +65,51 @@ else
     JAR_OPTS = cfe
 endif
 
-JAVAC_OPTS = $(LIBS_OPTS) -source 1.5 -target 1.5 -d "$(OUT_DIR)"
+JAVAC_OPTS = $(LIBS_OPTS) -source $(TARGET_VERSION) \
+    -target $(TARGET_VERSION) -d "$(OUT_DIR)"
 
+PKGS_PROPER = $(subst /,.,$(PKGS))
+
+JAVADOC_OPTS = -source $(TARGET_VERSION) -sourcepath "$(SRC_DIR)/" \
+    -protected -charset UTF-8 \
+    -keywords -sourcetab 4 -d "$(DOCS_DIR)" $(PKGS_PROPER)
+
+###
 ### targets
+###
+.PHONY: all docs printFlags clean
+
 all: printFlags $(JAR_FILE_NAME)
 
-### rules
+docs: $(SRCS)
+	@mkdir -p "$(DOCS_DIR)"
+	javadoc $(JAVADOC_OPTS)
+
 printFlags:
 	@mkdir -p "$(OUT_DIR)"
 	@echo "Compiling using: javac $(JAVAC_OPTS)"
 
+clean:
+	rm -rf "$(OUT_DIR)" "$(JAR_FILE_NAME)" *~ 
+
+###
+### rules
+###
+
 ### This indicates do not remove intermediate files
 .SECONDARY:
 
+### This expands the external jar files into bin for later combination
 $(OUT_DIR)/%.xxx: %.jar  
 	@mkdir -p "$(dir $(OUT_DIR)/$*.xxx)"
 	@echo -n > "$@"
 	cd "$(OUT_DIR)"; jar -xf "../$<"
 
-$(OUT_DIR)/%.class: src/%.java $(LIBS_TARGETS)
+$(OUT_DIR)/%.class: $(SRC_DIR)/%.java $(LIBS_TARGETS)
 	@echo "Compiling \"$<\" ..."
 	@javac $(JAVAC_OPTS) $< 
 # $(?:src/%=%)
 
 $(JAR_FILE_NAME): $(CLASSES) 
 	jar $(JAR_OPTS) "$@" $(MAIN_CLASS) -C "$(OUT_DIR)" .
-
-clean:
-	rm -rf "$(OUT_DIR)" "$(JAR_FILE_NAME)" *~ 
 
