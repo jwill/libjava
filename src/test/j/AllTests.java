afea49dd3b6f15314c48bd6ce738d55fdec935d8
Lucas Tan <tbhtan3@gmail.com>
2013-01-13 15:48:58 +0800

    added AllTests

---
+++ src/test/j/AllTests.java
@@ -0,0 +1,13 @@
+package j;
+
+import org.junit.extensions.cpsuite.*;
+import org.junit.extensions.cpsuite.ClasspathSuite.*;
+import org.junit.runner.RunWith;
+
+@RunWith(ClasspathSuite.class)
+@IncludeJars(true)
+public class AllTests 
+{
+    // nothing
+}
+
