0f8d72f0f8089e62485f0b2f25a6f9be083f8e65
Lucas Tan <tbhtan3@gmail.com>
2013-01-27 16:27:42 +0800

    added shuffle method for range

--- src/main/j/util/Util.java
+++ src/main/j/util/Util.java
@@ -35,10 +35,30 @@ public final class Util
      */
     public static <T> void shuffle(T[] array)
     {
+        // must test, otherwise end < start
+        if (array.length > 0)
+            shuffle(array, 0, array.length - 1);
+    }
+
+    /**
+     * Shuffles a contiguous part of an array in-place, with each possible 
+     * permutation having an equal chance of being the outcome.
+     * @param array This array should only have unique elements.
+     * @param start Zero-based index of the first element, inclusive.
+     * @param end Zero-based index of the last element, inclusive.
+     * @exception NullPointerException if array is null
+     * @exception IllegalArgumentException if end is lesser than start
+     * @exception IndexOutOfBoundsException if start or end is out of bounds
+     */
+    public static <T> void shuffle(T[] array, int start, int end)
+    {
+        if (end < start) throw new IllegalArgumentException("end < start");
+        
         final Random rand = getRandom();
-        for (int i = 0; i < array.length - 1; i++)
+
+        for (int i = start; i < end; i++)
         {
-            final int idx2 = rand.nextInt(array.length - i) + i;
+            final int idx2 = rand.nextInt((end - i) + 1) + i;
 
             // Swap
             final T tmp = array[i];
@@ -47,6 +67,23 @@ public final class Util
         }
     }
 
+    /**
+     * Shuffle method for a primitive int array.
+     */
+    public static void shuffle(int[] array)
+    {
+        final Random rand = getRandom();
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            final int idx2 = rand.nextInt(array.length - i) + i;
+
+            // Swap
+            final int tmp = array[i];
+            array[i] = array[idx2];
+            array[idx2] = tmp;
+        }
+    }
+    
     /**
      * Shuffle method for a primitive char array.
      */
